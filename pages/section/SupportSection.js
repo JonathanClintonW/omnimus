a41466b3d9894ccdbc518cd80a79768a7fce27f4
JonathanClintonW <jonathan.winatra@gmail.com>
2024-07-23 23:28:25 +0700

    Fix support box responsive

--- pages/section/SupportSection.js
+++ pages/section/SupportSection.js
@@ -22,9 +22,9 @@ const SupportSection = () => {
             <div className="w-full flex items-center font-bold text-lg border-b-4 border-black dark:border-white py-4 mb-8">
                 <h2>OUR SUPPORT</h2>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="flex flex-col lg:flex-row gap-8 items-center justify-around w-full">
                 {details.map((detail) => (
-                    <div className="bg-transparent shadow-md rounded-lg p-4 text-center border-2 border-turqoise hover:border-black dark:hover:border-white transition duration-500">  
+                    <div className="bg-transparent shadow-md max-w-md rounded-lg p-4 text-center border-2 border-turqoise hover:border-black dark:hover:border-white transition duration-500">  
                         <h3 className="text-xl font-bold pb-4 mb-2 border-b-2">{detail.title}</h3>
                         <p className="font-semibold">{detail.description}</p>
                     </div>
